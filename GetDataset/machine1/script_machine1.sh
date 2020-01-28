d242fc13985b42a626b88a5c399979bb0f219296
Gerardo Moreno <gmoreno9617@gmail.com>
2020-01-28 15:51:01 +0000

    Added error handling

--- GetDataset/machine1/script_machine1.sh
+++ GetDataset/machine1/script_machine1.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 x=1
 #make 5000 calls to the api (github's limit)
 while [ $x -le 5000 ]
 do
-  number_of_file=`tail -n 1 GitHub_TimeSeriesAnalysis/GetDataset/machine1/numberOfFile_1.txt`
-  id_number=`tail -n 1 GitHub_TimeSeriesAnalysis/GetDataset/machine1/idNumber_1.txt`
+  number_of_file=`tail -n 1 GitHub_TimeSeriesAnalysis/GetDataset/machine1/numberOfFile_1.txt 2>>log.txt`
+  id_number=`tail -n 1 GitHub_TimeSeriesAnalysis/GetDataset/machine1/idNumber_1.txt 2>>log.txt`
   if [ $id_number -le 74337497 ] #74337497+74337497+74337496
   then
-    curl -H "Authorization: token TOKEN" https://api.github.com/repositories?since=$id_number | grep '\"id\":\|full_name' >> GitHub_TimeSeriesAnalysis/GetDataset/machine1/reposLists_1/reposList$number_of_file.txt
-    python3 GitHub_TimeSeriesAnalysis/GetDataset/getID.py 1 $number_of_file >> GitHub_TimeSeriesAnalysis/GetDataset/machine1/idNumber_1.txt
+    curl -H "Authorization: token TOKEN" https://api.github.com/repositories?since=$id_number | grep '\"id\":\|full_name' >> GitHub_TimeSeriesAnalysis/GetDataset/machine1/reposLists_1/reposList$number_of_file.txt 2>>log.txt
+    python3 GitHub_TimeSeriesAnalysis/GetDataset/getID.py 1 $number_of_file >> GitHub_TimeSeriesAnalysis/GetDataset/machine1/idNumber_1.txt 2>>log.txt
     x=$(( $x + 1 ))
   else
     break
@@ -18,11 +18,11 @@ done
 if [ $id_number -le 74337497 ]
 then
   #clean repos list to obtain only the full_name
-  python3 GitHub_TimeSeriesAnalysis/GetDataset/getFullName.py 1 $number_of_file >> GitHub_TimeSeriesAnalysis/GetDataset/machine1/fullNames_1/fullNameList$number_of_file.txt
+  python3 GitHub_TimeSeriesAnalysis/GetDataset/getFullName.py 1 $number_of_file >> GitHub_TimeSeriesAnalysis/GetDataset/machine1/fullNames_1/fullNameList$number_of_file.txt 2>>log.txt
 
   #remember to use abosulte paths
   number_of_file=$(( $number_of_file + 1 ))
-  echo $number_of_file >> GitHub_TimeSeriesAnalysis/GetDataset/machine1/numberOfFile_1.txt
+  echo $number_of_file >> GitHub_TimeSeriesAnalysis/GetDataset/machine1/numberOfFile_1.txt 2>>log.txt
 else 
-  echo "Script 1 finished" >> GitHub_TimeSeriesAnalysis/GetDataset/results.txt
+  echo "Script 1 finished" >> GitHub_TimeSeriesAnalysis/GetDataset/results.txt 2>>log.txt
 fi
